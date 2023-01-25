8f664a8d41009f6a4408ae48ddaacb0653937359
tiyanicreselda <tiyanicreselda050@gmail.com>
2023-01-25 12:25:00 +0200

    Add project files.

---
+++ Creselda'sPolymorphism/Program.cs
@@ -0,0 +1,36 @@
+﻿class Animal   
+{
+    public virtual void animalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+
+class Pig : Animal  {
+    public override void animalSound()
+    {
+        Console.WriteLine("The pig says: wee wee");
+    }
+}
+
+class Dog : Animal   
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();  
+        Animal myPig = new Pig();  
+        Animal myDog = new Dog();  
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+}
